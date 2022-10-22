d211a23424f10760cd5b2c6d26a4c528175f90f3
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-10-22 16:19:21 -0600

    Working Out the Bugs

--- src/components/pages/Upload/RenderUpload.js
+++ src/components/pages/Upload/RenderUpload.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import ReactS3 from 'react-s3';
+import ReactS3, { uploadFile } from 'react-s3';
+import theKeys from '../../../.keys';
 
 const config = {
-  bucketName: '',
-  albumName: '',
-  AccessKeyId: '',
-  secretAccessKey: '',
+  bucketName: 'bucketeer-0fcbcf8b-2b04-4ddb-ac9d-29c05b9d4990',
+  region: 'us-east-1',
+  accessKeyId: theKeys.accessKeyId,
+  secretAccessKey: theKeys.secretAccessKey,
 };
+
+function upload(e) {
+  console.log(e.target.files[0]);
+  uploadFile(e.target.files[0], config)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      alert(err);
+    });
+}
 
 function RenderUpload(props) {
   return (
     <div>
       <h1>Upload Files Here</h1>
-      <div></div>
+      <div>
+        <input type="file" onChange={upload} />
+      </div>
     </div>
   );
 }
