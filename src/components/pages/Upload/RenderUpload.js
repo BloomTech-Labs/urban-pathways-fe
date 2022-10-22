c95048326c11d71cd169e1ece4c59b766377ce71
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-10-22 17:23:37 -0600

    closer

--- src/components/pages/Upload/RenderUpload.js
+++ src/components/pages/Upload/RenderUpload.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactS3, { uploadFile } from 'react-s3';
-import theKeys from '../../../.keys';
+import keyId from '../../../.keys';
+import secret from '../../../.keys';
 
 const config = {
   bucketName: 'bucketeer-0fcbcf8b-2b04-4ddb-ac9d-29c05b9d4990',
+  albumName: 'test',
   region: 'us-east-1',
-  accessKeyId: theKeys.keyId,
-  secretAccessKey: theKeys.secret,
+  accessKeyId: keyId,
+  secretAccessKey: secret,
 };
 console.log(config);
 function upload(e) {
   console.log(e.target.files[0]);
   uploadFile(e.target.files[0], config)
     .then(data => {
-      console.log(data.location);
+      console.log(data);
     })
     .catch(err => {
       console.log(err);
