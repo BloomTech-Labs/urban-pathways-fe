6dfa808d504d26191e10707e16ecd8f4736c1b31
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-10-22 15:24:04 -0600

    testing key protection

--- src/components/pages/Upload/RenderUpload.js
+++ src/components/pages/Upload/RenderUpload.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReactS3 from 'react-s3';
+
+const config = {
+  bucketName: '',
+  albumName: '',
+  AccessKeyId: '',
+  secretAccessKey: '',
+};
 
 function RenderUpload(props) {
   return (
     <div>
       <h1>Upload Files Here</h1>
+      <div></div>
     </div>
   );
 }
