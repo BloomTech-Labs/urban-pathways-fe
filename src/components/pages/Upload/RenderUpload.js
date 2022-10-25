a43a325356f081f0d4ae5c0adb96451a73d58b2a
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-10-24 21:52:41 -0600

    Getting rid of junk

--- src/components/pages/Upload/RenderUpload.js
+++ src/components/pages/Upload/RenderUpload.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import ReactS3, { uploadFile } from 'react-s3';
-import keyId from '../../../.keys';
-import secret from '../../../.keys';
 
-const config = {
-  bucketName: 'bucketeer-0fcbcf8b-2b04-4ddb-ac9d-29c05b9d4990',
-  albumName: 'test',
-  region: 'us-east-1',
-  accessKeyId: keyId,
-  secretAccessKey: secret,
-};
-console.log(config);
 function upload(e) {
   console.log(e.target.files[0]);
-  uploadFile(e.target.files[0], config)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  console.log(Date.now());
 }
 
 function RenderUpload(props) {
